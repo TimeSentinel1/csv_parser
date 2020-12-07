ca45991131140261cd1969511c6ccf1ddc779f18
unknown <djepse20@student.aau.dk>
2020-12-07 06:10:05 +0100

    ændrede csv parseren så der er mulighed for at ændre hvilken delimiter den bruger

--- csv.c
+++ csv.c
@@ -12,7 +12,7 @@ void free_csv_line( char **parsed ) {
     free( parsed );
 }
 
-static int count_fields( const char *line ) {
+static int count_fields( const char *line, const char ch) {
     const char *ptr;
     int cnt, fQuote;
 
@@ -27,17 +27,18 @@ static int count_fields( const char *line ) {
             }
             continue;
         }
-
-        switch( *ptr ) {
-            case '\"':
-                fQuote = 1;
-                continue;
-            case ',':
-                cnt++;
-                continue;
-            default:
-                continue;
+        if(*ptr == '\"')
+        {
+             fQuote = 1;
+            continue;
+        } else if(*ptr == ch){
+            cnt++;
+            continue;
+        } else {
+            continue;
         }
+
+   
     }
 
     if ( fQuote ) {
@@ -52,12 +53,12 @@ static int count_fields( const char *line ) {
  *  which are escaped by "double quotes", extract a NULL-terminated
  *  array of strings, one for every cell in the row.
  */
-char **parse_csv( const char *line ) {
+char **parse_csv( const char *line,const char ch) {
     char **buf, **bptr, *tmp, *tptr;
     const char *ptr;
     int fieldcnt, fQuote, fEnd;
 
-    fieldcnt = count_fields( line );
+    fieldcnt = count_fields( line,ch);
 
     if ( fieldcnt == -1 ) {
         return NULL;
@@ -98,15 +99,16 @@ char **parse_csv( const char *line ) {
 
             continue;
         }
-
-        switch( *ptr ) {
-            case '\"':
+            if(*ptr == '\"')
+            {
                 fQuote = 1;
                 continue;
-            case '\0':
+            } else if (*ptr == '\0')
+            {
                 fEnd = 1;
-            case ',':
-                *tptr = '\0';
+            } else if(*ptr == ch)
+            {
+                     *tptr = '\0';
                 *bptr = StringDup( tmp );
 
                 if ( !*bptr ) {
@@ -123,15 +125,16 @@ char **parse_csv( const char *line ) {
                 tptr = tmp;
 
                 if ( fEnd ) {
-                  break;
+
                 } else {
                   continue;
                 }
-
-            default:
-                *tptr++ = *ptr;
+            } else {
+                 *tptr++ = *ptr;
                 continue;
-        }
+            }
+
+            
 
         if ( fEnd ) {
             break;
--- csv.h
+++ csv.h
@@ -6,7 +6,7 @@
 #define CSV_ERR_LONGLINE 0
 #define CSV_ERR_NO_MEMORY 1
 
-char **parse_csv( const char *line );
+char **parse_csv( const char *line, const char ch);
 char* StringDup(char *src);
 void free_csv_line( char **parsed );
 char **split_on_unescaped_newlines(const char *txt);
